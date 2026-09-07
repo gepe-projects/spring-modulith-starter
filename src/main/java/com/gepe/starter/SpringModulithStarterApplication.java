0d556a3aa8942db0eae5c1637285a9487d429941
ilhamgepe <gepedevelop@gmail.com>
2026-09-07 12:48:40 +0700

    first commit

---
+++ src/main/java/com/gepe/starter/SpringModulithStarterApplication.java
@@ -0,0 +1,13 @@
+package com.gepe.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModulithStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringModulithStarterApplication.class, args);
+    }
+
+}
